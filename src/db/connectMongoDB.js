54bd4d97c9fafc85e0aea55d50bd2883a065482f
KarllWest <karlwest396@gmail.com>
2026-03-22 16:10:09 +0000

    fix: add error handling in connectMongoDB, use returnDocument after in updateNote

--- src/db/connectMongoDB.js
+++ src/db/connectMongoDB.js
@@ -1,7 +1,12 @@
 import mongoose from 'mongoose';
 
 export const connectMongoDB = async () => {
-  const url = process.env.MONGO_URL;
-  await mongoose.connect(url);
-  console.log('✅ MongoDB connection established successfully');
+  try {
+    const url = process.env.MONGO_URL;
+    await mongoose.connect(url);
+    console.log('✅ MongoDB connection established successfully');
+  } catch (err) {
+    console.error('❌ MongoDB connection failed:', err.message);
+    process.exit(1);
+  }
 };
